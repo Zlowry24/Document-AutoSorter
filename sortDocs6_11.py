eb5dbfdaa9933e400fa9f51795c8a5f53b209f70
Zlowry24 <lowryzachery06@gmail.com>
2025-06-23 14:27:09 -0400

    Update sortDocs6_11.py

--- sortDocs6_11.py
+++ sortDocs6_11.py
@@ -2,7 +2,7 @@ from operator import truediv
 from pathlib import Path  #imports the path class from pathlib python package
 from docx import Document #imports Document class from PyDocx package
 
-folder_path = Path(r"C:\Users\lowry\Desktop\Unsorted Documents") #points to folder, rstring prevents backspace from causing issues
+folder_path = Path(r"C:\Users\MyName\Desktop\Unsorted Documents") #points to folder, rstring prevents backspace from causing issues
 files = folder_path.iterdir()  #Returns an iterator of path objects for each file/ folder in Unsorted documents (.iterdir is from the path class
 for file in files: #loops through files variable(iterator of path objects) temp naming each one file
     print(file)
